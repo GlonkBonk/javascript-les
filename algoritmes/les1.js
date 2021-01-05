9a597c0861266660d98d19e084a59ef0d28e1d18
Giotje <devgio@icloud.com>
2021-01-05 21:13:30 +0100

    added algoritm les1

---
+++ algoritmes/les1.js
@@ -0,0 +1,15 @@
+/*
+ * In this assignment you'll need to return the difference between the two
+ * array's and put those changes into an array called 'difference'.
+ */
+
+var arr_one = ['koen', 'jan', 'peter']
+var arr_two = ['koen', 'jan', 'peter', 'diederik', 'thomas']
+
+arr_diff = (arr_one, arr_two) =>{
+  var  a = [], diff = [];
+  //The rest is for you to find out :)
+}
+
+console.log(arr_diff(arr_one, arr_two))
+
